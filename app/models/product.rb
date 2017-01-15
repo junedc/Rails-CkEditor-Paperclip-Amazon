10a3004358ed68dc3d5f7530aa820cec1f5e8685
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-01-15 20:39:25 +0800

    Modify product model to support image attachment.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    preview: '300x225#',
+    large: '600x600>'
+  }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
